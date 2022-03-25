614262ca8c5e63b920f308e1091ec5f801ee46e3
khoanguyenanhhd <khoa.nguyenanh@hdwebsoft.io>
2022-03-25 09:11:40 +0700

    improve transaction when request multiple

--- src/modules/eventTracking/eventTracking.ts
+++ src/modules/eventTracking/eventTracking.ts
@@ -9,6 +9,7 @@ import { shouldRetryTransaction } from "../../utils/helper";
 export class EventTrackingResolver {
   private connection = getConnection();
   private eventRepository = getRepository(Event);
+  private eventTrackingRepository = getRepository(EventTracking);
 
   @Authorized()
   @Mutation(() => String)
@@ -22,97 +23,82 @@ export class EventTrackingResolver {
       throw new Error("Event not found");
     }
 
-    const queryRunner = this.connection.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction("SERIALIZABLE");
-
+    let done = false;
+    let retryCount = 0;
     let isLockedByAnotherUser = false;
     let isLockedByCurrentUser = false;
     let isUserEditAnotherEvent = false;
+    while (!done && retryCount < 5) {
+      retryCount++;
 
-    try {
-      const eventTrackingManager =
-        queryRunner.manager.getRepository("EventTracking");
+      const queryRunner = this.connection.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction("SERIALIZABLE");
 
-      const eventTracking = (await eventTrackingManager.findOne({
-        where: { event: { id: event.id } },
-        order: { createdAt: "DESC" },
-      })) as EventTracking;
+      try {
+        const eventTrackingManager =
+          queryRunner.manager.getRepository("EventTracking");
 
-      const isUserEditing = (await eventTrackingManager.findOne({
-        where: { userId },
-        order: { createdAt: "DESC" },
-      })) as EventTracking;
+        const eventTracking = (await eventTrackingManager.findOne({
+          where: { event: { id: event.id } },
+          order: { createdAt: "DESC" },
+        })) as EventTracking;
 
-      // console.log("Read oke");
-      // console.log(eventTracking);
+        const isUserEditing = (await eventTrackingManager.findOne({
+          where: { userId },
+          order: { createdAt: "DESC" },
+        })) as EventTracking;
 
-      if (
-        isUserEditing &&
-        isUserEditing.releaseAt > new Date() &&
-        eventTracking.id !== isUserEditing.id
-      ) {
-        isUserEditAnotherEvent = true;
-      } else if (
-        eventTracking &&
-        eventTracking.userId !== userId &&
-        eventTracking.releaseAt > new Date()
-      ) {
-        isLockedByAnotherUser = true;
-      } else if (
-        eventTracking &&
-        eventTracking.userId === userId &&
-        eventTracking.releaseAt > new Date()
-      ) {
-        isLockedByCurrentUser = true;
-      } else {
-        const lockedAt = new Date();
-        const releaseAt = new Date(lockedAt);
-        releaseAt.setMinutes(releaseAt.getMinutes() + 5);
-
-        const newEventTracking = eventTrackingManager.create({
-          lockedAt,
-          releaseAt,
-          userId,
-          event: event,
-        }) as EventTracking;
-
-        await eventTrackingManager.save(newEventTracking);
-        await queryRunner.commitTransaction();
-
-        return "Access successfully";
-      }
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-
-      // Handle write conflict
-      if (shouldRetryTransaction(error)) {
-        const eventTracking = (await queryRunner.manager
-          .getRepository("EventTracking")
-          .findOne({
-            where: { event: { id: event.id } },
-            order: { createdAt: "DESC" },
-          })) as EventTracking;
+        // console.log("Read oke");
+        // console.log(eventTracking);
 
         if (
+          isUserEditing &&
+          isUserEditing.releaseAt > new Date() &&
+          eventTracking.id !== isUserEditing.id
+        ) {
+          isUserEditAnotherEvent = true;
+          done = true;
+        } else if (
           eventTracking &&
           eventTracking.userId !== userId &&
           eventTracking.releaseAt > new Date()
         ) {
           isLockedByAnotherUser = true;
+          done = true;
         } else if (
           eventTracking &&
           eventTracking.userId === userId &&
           eventTracking.releaseAt > new Date()
         ) {
           isLockedByCurrentUser = true;
+          done = true;
+        } else {
+          const lockedAt = new Date();
+          const releaseAt = new Date(lockedAt);
+          releaseAt.setMinutes(releaseAt.getMinutes() + 5);
+
+          const newEventTracking = eventTrackingManager.create({
+            lockedAt,
+            releaseAt,
+            userId,
+            event: event,
+          }) as EventTracking;
+
+          await eventTrackingManager.save(newEventTracking);
+          await queryRunner.commitTransaction();
+
+          done = true;
+
+          return "Access successfully";
         }
-      } else {
-        throw error;
+      } catch (error) {
+        await queryRunner.rollbackTransaction();
+      } finally {
+        await queryRunner.release();
       }
-    } finally {
-      await queryRunner.release();
     }
+    if (retryCount === 5) return "Access fail, please try again";
 
     if (isUserEditAnotherEvent) return "One user can edit one event";
 
@@ -133,62 +119,26 @@ export class EventTrackingResolver {
       throw new Error("Event not found");
     }
 
-    const queryRunner = this.connection.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction("SERIALIZABLE");
-
+    let done = false;
+    let retryCount = 0;
     let isLockedByAnotherUserAndOverTime = false;
     let isLockedByAnotherUserAndInTime = false;
     let isLockedByCurrentUserAndOverTime = false;
 
-    try {
-      const eventTrackingManager =
-        queryRunner.manager.getRepository("EventTracking");
+    while (!done && retryCount < 5) {
+      retryCount++;
+      const queryRunner = this.connection.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction("SERIALIZABLE");
 
-      const eventTracking = (await eventTrackingManager.findOne({
-        where: { event: { id: event.id } },
-        order: { createdAt: "DESC" },
-      })) as EventTracking;
+      try {
+        const eventTrackingManager =
+          queryRunner.manager.getRepository("EventTracking");
 
-      if (
-        (eventTracking &&
-          eventTracking.userId !== userId &&
-          eventTracking.releaseAt < new Date()) ||
-        !eventTracking
-      ) {
-        isLockedByAnotherUserAndOverTime = true;
-      } else if (
-        eventTracking &&
-        eventTracking.userId !== userId &&
-        eventTracking.releaseAt > new Date()
-      ) {
-        isLockedByAnotherUserAndInTime = true;
-      } else if (
-        eventTracking &&
-        eventTracking.userId === userId &&
-        eventTracking.releaseAt < new Date()
-      ) {
-        isLockedByCurrentUserAndOverTime = true;
-      } else {
-        const releaseAt = new Date();
-
-        eventTracking.releaseAt = releaseAt;
-
-        await eventTrackingManager.save(eventTracking);
-        await queryRunner.commitTransaction();
-
-        return "Release successfully";
-      }
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-
-      if (shouldRetryTransaction(error)) {
-        const eventTracking = (await queryRunner.manager
-          .getRepository("EventTracking")
-          .findOne({
-            where: { event: { id: event.id } },
-            order: { createdAt: "DESC" },
-          })) as EventTracking;
+        const eventTracking = (await eventTrackingManager.findOne({
+          where: { event: { id: event.id } },
+          order: { createdAt: "DESC" },
+        })) as EventTracking;
 
         if (
           (eventTracking &&
@@ -197,25 +147,41 @@ export class EventTrackingResolver {
           !eventTracking
         ) {
           isLockedByAnotherUserAndOverTime = true;
+          done = true;
         } else if (
           eventTracking &&
           eventTracking.userId !== userId &&
           eventTracking.releaseAt > new Date()
         ) {
           isLockedByAnotherUserAndInTime = true;
+          done = true;
         } else if (
           eventTracking &&
           eventTracking.userId === userId &&
           eventTracking.releaseAt < new Date()
         ) {
           isLockedByCurrentUserAndOverTime = true;
+          done = true;
+        } else {
+          const releaseAt = new Date();
+
+          eventTracking.releaseAt = releaseAt;
+
+          await eventTrackingManager.save(eventTracking);
+          await queryRunner.commitTransaction();
+
+          done = true;
+
+          return "Release successfully";
         }
-      } else {
-        throw error;
+      } catch (error) {
+        await queryRunner.rollbackTransaction();
+      } finally {
+        await queryRunner.release();
       }
-    } finally {
-      await queryRunner.release();
     }
+
+    if (retryCount === 5) return "Something went wrong, please try again";
 
     if (isLockedByAnotherUserAndOverTime)
       return "Need to access before releasing";
@@ -239,60 +205,72 @@ export class EventTrackingResolver {
       throw new Error("Event not found");
     }
 
-    const queryRunner = this.connection.createQueryRunner();
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
-
+    let done = false;
+    let retryCount = 0;
     let isLockedByAnotherUserAndOverTime = false;
     let isLockedByAnotherUserAndInTime = false;
     let isLockedByCurrentUserAndOverTime = false;
 
-    try {
-      const eventTrackingManager =
-        queryRunner.manager.getRepository("EventTracking");
+    while (!done && retryCount < 5) {
+      retryCount++;
 
-      const eventTracking = (await eventTrackingManager.findOne({
-        where: { event: { id: event.id } },
-        order: { createdAt: "DESC" },
-        lock: { mode: "pessimistic_write" },
-      })) as EventTracking;
+      const queryRunner = this.connection.createQueryRunner();
+      await queryRunner.connect();
+      await queryRunner.startTransaction("SERIALIZABLE");
 
-      if (
-        (eventTracking &&
+      try {
+        const eventTrackingManager =
+          queryRunner.manager.getRepository("EventTracking");
+
+        const eventTracking = (await eventTrackingManager.findOne({
+          where: { event: { id: event.id } },
+          order: { createdAt: "DESC" },
+        })) as EventTracking;
+
+        if (
+          (eventTracking &&
+            eventTracking.userId !== userId &&
+            eventTracking.releaseAt < new Date()) ||
+          !eventTracking
+        ) {
+          isLockedByAnotherUserAndOverTime = true;
+          done = true;
+        } else if (
+          eventTracking &&
           eventTracking.userId !== userId &&
-          eventTracking.releaseAt < new Date()) ||
-        !eventTracking
-      ) {
-        isLockedByAnotherUserAndOverTime = true;
-      } else if (
-        eventTracking &&
-        eventTracking.userId !== userId &&
-        eventTracking.releaseAt > new Date()
-      ) {
-        isLockedByAnotherUserAndInTime = true;
-      } else if (
-        eventTracking &&
-        eventTracking.userId === userId &&
-        eventTracking.releaseAt < new Date()
-      ) {
-        isLockedByCurrentUserAndOverTime = true;
-      } else {
-        const releaseAt = new Date();
-        releaseAt.setMinutes(releaseAt.getMinutes() + 5);
+          eventTracking.releaseAt > new Date()
+        ) {
+          isLockedByAnotherUserAndInTime = true;
+          done = true;
+        } else if (
+          eventTracking &&
+          eventTracking.userId === userId &&
+          eventTracking.releaseAt < new Date()
+        ) {
+          isLockedByCurrentUserAndOverTime = true;
+          done = true;
+        } else {
+          const releaseAt = new Date();
+          releaseAt.setMinutes(releaseAt.getMinutes() + 5);
 
-        eventTracking.releaseAt = releaseAt;
-        eventTracking.maintainTime = Number(eventTracking.maintainTime) + 1;
+          eventTracking.releaseAt = releaseAt;
+          eventTracking.maintainTime = Number(eventTracking.maintainTime) + 1;
 
-        await eventTrackingManager.save(eventTracking);
-        await queryRunner.commitTransaction();
+          await eventTrackingManager.save(eventTracking);
+          await queryRunner.commitTransaction();
 
-        return "Maintain successfully";
+          done = true;
+
+          return "Maintain successfully";
+        }
+      } catch (error) {
+        await queryRunner.rollbackTransaction();
+      } finally {
+        await queryRunner.release();
       }
-    } catch (error) {
-      await queryRunner.rollbackTransaction();
-    } finally {
-      await queryRunner.release();
     }
+
+    if (retryCount === 5) return "Something went wrong, please try again";
 
     if (isLockedByAnotherUserAndOverTime)
       return "Need to access before maintain";
